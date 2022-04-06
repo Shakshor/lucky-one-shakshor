837aa2abf3374febb6021b9e2bcf2343e563ad1b
Shakshor <shakshor584@gmail.com>
2022-04-06 10:44:31 +0600

    fake data generate done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
+
     return (
         <div className='shop-container'>
             <div className='products-container'>
